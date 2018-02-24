f6d325537ff9a649198a3a58cfcd10fba31b5fab
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-24 13:59:45 -0700

    Validations set in event model, validation testing in event spec, test passes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates_presence_of :name, uniqueness: true
+  validates_presence_of :organizer
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    scenario "event is not valid without name" do
+      event = Event.new(organizer: "sam")
+
+      expect(event).to_not be_valid
+    end
+
+    scenario "event is not valid without organizer" do
+      event = Event.new(name: "sam")
+
+      expect(event).to_not be_valid
+    end
+
+    scenario " event is valid with organizer and name" do
+      event = Event.new(name: "SAM", organizer: "SAM")
+
+      expect(event).to be_valid
+    end
+  end
 end
