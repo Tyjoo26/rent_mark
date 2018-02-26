508cedf842c55b4e984dea86d7cf52f94ccc25c9
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 15:11:44 -0700

    user can seee dashboard spec test passes

--- spec/features/user_can_see_dashboard_spec.rb
+++ spec/features/user_can_see_dashboard_spec.rb
@@ -1,11 +1,15 @@
 describe "as a user" do
   context "when i log in" do
     scenario "I see my account details, my rent" do
-      user = create(:user)
+      unit = create(:unit, rent: 100, unit_type: "1bdr")
+      user = create(:user, unit: unit)
+
       stub_logged_in_user(user)
       visit dashboard_path
-      # binding.pry
+
       expect(page).to have_content("Welcome, #{user.first_name}!")
+      expect(page).to have_content("Rent: $#{user.unit.rent}")
+      expect(page).to have_content("Rent is due: #{user.unit.rent_due_date}")
     end
   end
 end
