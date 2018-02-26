c33c0eb6ba87955fb53df99b533b7fa22d200e05
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 08:25:57 -0700

    add helper methods for applciation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+  
 end
