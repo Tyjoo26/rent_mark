59b8c289796776389c85f75fac719a0e384a13da
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 13:43:57 -0700

    finished oauth test spec, test passes, removed event spec tests

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -6,16 +6,26 @@ RSpec.feature 'user logs in' do
     expect(page).to have_link("Sign in with Google")
     click_link("Sign in with Google")
 
-    expect(page).to have_content("Timothy Joo")
-    expect(page).to have_link("Log Out")
+  
+    expect(page).to have_link("Logout")
   end
 end
 
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-
-      })
-
+      provider: "google",
+      uid: "12345678910",
+      info: {
+        email: "tyjoo21@gmail.com",
+        first_name: "Tim",
+        last_name: "Joo"
+      },
+      credentials: {
+        token: "abcdefg12345",
+        refresh_token: "12345abcdefg",
+        expires_at: DateTime.now,
+      }
+    })
 
   end
