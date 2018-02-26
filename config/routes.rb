d173527f1e4e945dae2792e59d61f943f0af173a
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 12:34:19 -0700

    Add custom routes for contact me form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'auth/failure',            to: redirect('/')
   get 'logout',                  to: 'sessions#destroy'
 
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
+
   resource :dashboard, only: :show
   resource :users, only: [:show, :update]
   get "account/edit", to: "users#edit", as: 'edit_user'
