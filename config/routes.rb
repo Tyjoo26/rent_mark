4cff51fa57aaa984bad2a50b771fea0f6a3fc4bc
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 08:45:57 -0700

    modify routes add login and callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  get "auth/google/callback",  to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
+  get "auth/google/callback",    to: 'sessions#create'
+  get 'auth/failure',            to: redirect('/')
+  get 'logout',                  to: 'sessions#destroy'
 
   resource :dashboard, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_26_151717) do
+ActiveRecord::Schema.define(version: 2018_02_26_153556) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
