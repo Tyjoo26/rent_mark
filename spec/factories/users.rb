947e757d7595087dc97418311e302a08ea39f4b8
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 13:20:49 -0700

    make factory bot user more robust, test for validations in user spec to see if fake rworks, add require factory bot in rails_helper

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+
+
 FactoryBot.define do
   factory :user do
-    provider "MyString"
-    first_name "MyString"
-    last_name "MyString"
+    provider "google"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
     token "MyString"
     refresh_token "MyString"
     oauth_expires_at "2018-02-26 08:17:32"
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
   context "validations" do
+    it "is valid with first_name and last_name" do
+      user = build(:user)
 
+      expect(user).to be_valid
+    end
   end
 end
