893810658153611a3bc8f951be9cd28eda318e88
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 08:27:06 -0700

    modify routes for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  get 'auth/google/callback',  to: 'sessions#create'
+
+  get "auth/google/callback",  to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
+  resource :dashboard, only: :show
 end
