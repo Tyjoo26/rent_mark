9c4effe0e5c8c31276867c61593e43cfe37daa16
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 13:52:25 -0700

    fix feature helper module and got user can see dashboard spec to pass early tests

--- spec/features/user_can_see_dashboard_spec.rb
+++ spec/features/user_can_see_dashboard_spec.rb
@@ -1,9 +1,10 @@
 describe "as a user" do
   context "when i log in" do
     scenario "I see my account details, my rent" do
-      stub_logged_in_user(create(:user))
+      user = create(:user)
+      stub_logged_in_user(user)
       visit dashboard_path
-
+      # binding.pry
       expect(page).to have_content("Welcome, #{user.first_name}!")
     end
   end
