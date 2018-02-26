6b65bebda2bc29f93baf3126b3d6c1ed5b54a9fd
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 11:59:25 -0700

    modify initializer for scope email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["client_id"], ENV["client_secret"], {
     :name => "google",
-    :scope => ['contacts','plus.login','plus.me'],
+    :scope => ['contacts','plus.login','plus.me', 'userinfo.email'],
     :prompt => "select_account",
     :image_aspect_ratio => "square",
     :image_size => 50,
