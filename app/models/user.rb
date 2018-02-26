4cbb3582464e521f5418662476b6b0055186f619
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 08:26:31 -0700

    create find or update method"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone_number, presence: true, uniqueness: true
+
+
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+
+    user.attributes = {
+    provider: auth[:provider],
+    uid: auth[:uid],
+    first_name: auth[:info][:first_name],
+    last_name: auth[:info][:last_name],
+    token: auth[:credentials][:token],
+    refresh_token: auth[:credentials][:refresh_token],
+    oauth_expires_at: auth[:credentials][:expires_at]
+    }
+
+    user.save!
+    user 
+  end
 end
