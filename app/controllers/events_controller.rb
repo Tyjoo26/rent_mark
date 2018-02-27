2e1a14c9ffe5c55c9f83d973eca8b3144b72d297
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 20:24:09 -0700

    Build events controller with index and custom action

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,21 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def attend
+    @event = Event.find(params[:id])
+    if @event.users.include?(current_user) == true
+      flash[:alert] = "You've already RSVPed!"
+      redirect_to events_path
+    else @event.users << current_user
+      flash[:success] = "You've saved a spot!"
+      redirect_to events_path
+    end
+  end
+end
