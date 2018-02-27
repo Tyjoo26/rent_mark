70f5b080e55b68f81210b8949a94de1f3040da53
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 17:54:47 -0700

    validations for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "will be a valid user with first_name & last name" do
+      user = User.new(first_name: "123", last_name: "123")
+
+      expect(user).to be_valid
+    end
+
+    it "will not be valid user without last_name " do
+      user = User.new(first_name: "123")
+
+      expect(user).to_not be_valid
+    end
+
+    it "will not be valid user without first_name " do
+      user = User.new(last_name: "123")
+
+      expect(user).to_not be_valid
+    end
+  end
 end
