d3372a49669593b2e4b60dfa1a1b3b85a789a2d0
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 20:23:08 -0700

    Event spec validations test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations " do
+    it "is valid with name, details date" do
+      event = build(:event)
+
+      expect(event).to be_valid
+    end
+
+    it "is ivalid without name" do
+      event = Event.new(details: "string", date: Date.current)
+
+      expect(event).to_not be_valid
+    end
+
+    it "is ivalid without date" do
+      event = Event.new(details: "string", name: "shit")
+
+      expect(event).to_not be_valid
+    end
+
+    it "is ivalid without details" do
+      event = Event.new(name: "string", date: Date.current)
+
+      expect(event).to_not be_valid
+    end
+  end
 end
