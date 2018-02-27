deba45de32d774c09b0aef12cb9cdcb59c8e34e1
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-26 17:50:44 -0700

    if else logic for sessions create that include private methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
 
+  def new
+
+  end
+
   def create
-    user = User.update_or_create(request.env["omniauth.auth"])
-    session[:id] = user.id
-    redirect_to dashboard_path
+    if request.env["omniauth.auth"].nil?
+      @user = User.find_by(email: params[:session][:email])
+      verify_user
+    else
+      @user = User.update_or_create(request.env["omniauth.auth"])
+      session[:id] = @user.id
+      redirect_to dashboard_path
+    end
   end
 
 
@@ -11,4 +20,26 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  private
+
+    def verify_user
+      if @user.try(:authenticate, params[:session][:password])
+        login_successful
+      else
+        flash[:alert] = "Login Unsuccessful"
+        redirect_to login_path
+      end
+    end
+
+    def login_successful
+      session[:user_id] = @user.id
+      if current_user.manager?
+        redirect_to dashboard_path
+      else
+        redirect_to dashboard_path
+      end
+    end
+
+
 end
