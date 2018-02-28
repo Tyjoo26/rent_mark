bbadd09107ca4c4b24a06700c3aca839a9942702
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-28 15:00:39 -0700

    renamed env variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,4 @@
 Rails.configuration.stripe = {
   :stripe_key => ENV['stripe_key'],
-  :secret_key      => ENV['stripe_secret']
+  :api_key      => ENV['stripe_secret']
 }
-
-Stripe.api_key = ENV["stripe_secret"]
