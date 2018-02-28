40b5352630f00fb9a633b2444c1c7b4ffb66524b
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-27 21:57:45 -0700

    create event and unit controller for managers

---
+++ app/controllers/manager/events_controller.rb
@@ -0,0 +1,43 @@
+class Manager::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:name, :details, :date)
+    end
+
+
+end
---
+++ app/controllers/manager/units_controller.rb
@@ -0,0 +1,35 @@
+class Manager::UnitsController < ApplicationController
+  before_action :set_unit, only: [:show, :edit, :update]
+
+  def index
+    @units = Unit.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @unit.update(unit_params)
+      flash[:alert] = "You've updated #{@unit.unit_number}'s details!"
+      redirect_to manager_unit_path(@unit)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+    def set_unit
+      @unit = Unit.find(params[:id])
+    end
+
+    def unit_params
+      params.require(:unit).permit(:rent, :rent_due, :rent_due_date)
+    end
+end
