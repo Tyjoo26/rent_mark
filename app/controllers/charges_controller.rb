54cbc1084594fd4bb35d3a5bde592daa56d1c467
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-28 14:59:22 -0700

    refactored stripe create charges action to stripe service and moved api key to env

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,21 +6,9 @@ class ChargesController < ApplicationController
 
   def create
     @amount = (current_user.unit.rent_due * 100).round
-
-    Stripe.api_key = ENV["stripe_secret"]
-
+    customer = StripeService.initiate_customer(params[:stripeEmail], params[:stripeToken])
     begin
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source => params[:stripeToken]
-      )
-
-      charge = Stripe::Charge.create(
-        :customer => customer.id,
-        :amount => @amount,
-        :description => "#{current_user.unit.unit_number} Rent Payment",
-        :currency => 'usd'
-      )
+      StripeService.initiate_charge(customer, @amount, current_user.unit.unit_number)
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
