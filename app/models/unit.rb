616f2ef8a2c9b9e782d7f1767120d2db5e26c167
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-27 21:56:28 -0700

    change has_nany  to has One

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,7 +3,7 @@ class Unit < ApplicationRecord
   validates_presence_of :unit_number, :unit_type, :rent
   validates_uniqueness_of :unit_number
 
-  has_many :users
+  has_one :user
 
   def unit_due_date
     rent_due_date >> 1
