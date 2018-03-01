2e887eb2c69154d72b0f0d265570588a70e13135
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-28 19:51:14 -0700

    fix seeder to properly associate unit with user

--- db/seeder.rb
+++ db/seeder.rb
@@ -28,5 +28,73 @@ class Seeder
     Unit.create(unit_number: "B6", unit_type: "Studio", rent: 865.00, rent_due_date: Date.current, rent_due: 865.00)
     Unit.create(unit_number: "B7", unit_type: "Studio", rent: 865.00, rent_due_date: Date.current, rent_due: 865.00)
     Unit.create(unit_number: "B8", unit_type: "Studio", rent: 865.00, rent_due_date: Date.current, rent_due: 865.00)
+
+    u1 = Unit.find(1)
+    a = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u1.id)
+    a.save
+
+    u2 = Unit.find(2)
+    b = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u2.id)
+    b.save
+
+    u3 = Unit.find(3)
+    c = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u3.id)
+    c.save
+
+    u4 = Unit.find(4)
+    d = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u4.id)
+    d.save
+
+    u5 = Unit.find(5)
+    e = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u5.id)
+    e.save
+
+    u6 = Unit.find(6)
+    f = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u6.id)
+    f.save
+
+    u7 = Unit.find(7)
+    g = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u7.id)
+    g.save
+
+    u8 = Unit.find(8)
+    h = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u8.id)
+    h.save
+
+    u9 = Unit.find(9)
+    i = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u9.id)
+    i.save
+
+    u10 = Unit.find(10)
+    k = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u10.id)
+    k.save
+
+    u11 = Unit.find(11)
+    y = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u11.id)
+    y.save
+
+    u12 = Unit.find(12)
+    i = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u12.id)
+    i.save
+
+    u13 = Unit.find(13)
+    w = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u13.id)
+    w.save
+
+    u14 = Unit.find(14)
+    ba = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u14.id)
+    ba.save
+
+    u15 = Unit.find(15)
+    aa = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u15.id)
+    aa.save
+
+    u16 = Unit.find(16)
+    baw = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u16.id)
+    baw.save
+
+    u17 = Unit.find(17)
+    batrew= User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "123", unit_id: u17.id)
+    batrew.save
   end
 end
