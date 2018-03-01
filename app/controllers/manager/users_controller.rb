e0f9699884827444465f5ab663095e257faec2a4
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-28 20:25:48 -0700

    removed password params

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -1,5 +1,5 @@
 class Manager::UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.where(role: "renter")
@@ -24,6 +24,7 @@ class Manager::UsersController < ApplicationController
   end
 
   def update
+  
     if @user.update(user_params)
       flash[:success] = "You've updated #{@user.first_name}'s profile"
       redirect_to manager_users_path
@@ -41,7 +42,7 @@ class Manager::UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :unit_id, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 
     def set_user
